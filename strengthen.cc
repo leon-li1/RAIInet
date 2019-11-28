fba31033a365043efe9b28edd86499c30b5b829b
mpkkruge <mpkkruge@student.cs.uwaterloo.ca>
2019-11-28 16:55:21 -0500

    added strengthen ability

--- strengthen.cc
+++ strengthen.cc
@@ -3,4 +3,12 @@
 
 Strengthen::Strengthen(Player *owner, Player *other) : Ability{owner, other} {}
 
-void Strengthen::use(istream &in) {}
+void Strengthen::use(istream &in) {
+    std::string name;
+    int stren;
+    in >> name >> stren;
+    
+    Piece* p = owner->getPiece(name);
+    piece->setStrength(stren);
+
+}
