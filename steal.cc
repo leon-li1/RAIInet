ef8e626030250dfc24eab0b8f9397e5c5fb70e71
l475li <l475li@student.cs.uwaterloo.ca>
2019-11-28 16:48:22 -0500

    implemented steal.cc

--- steal.cc
+++ steal.cc
@@ -3,4 +3,15 @@
 
 Steal::Steal(Player *owner, Player *other) : Ability{owner, other} {}
 
-void Steal::use(istream &in) {}
+void Steal::use(istream &in) {
+
+    int myViruses = owner->getVirusCount();
+    int otherData = other->getDataCount();
+    
+    if (otherData > 0 &&  myViruses > 0) {
+        owner->setVirusCount(myViruses - 1);
+        other->setDataCount(otherData - 1);
+        other->setVirusCount(other->getvirusCount() + 1);
+        owner->setDataCount(owner->getDataCount() + 1);
+    }
+}
